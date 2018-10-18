e3c2a76dcadc1259f60e268f3af5960aebde2114
Unknown <shota_katayama@unirita.co.jp>
2018-10-18 14:42:56 +0900

    更新

--- 97991_ABC088C_Takahashi'sInformation.c
+++ 97991_ABC088C_Takahashi'sInformation.c
@@ -1,31 +1,47 @@
+/*
+
+97991_片山翔太 Ver.1.00 2018_10_16
+	作成
+97991_片山翔太 Ver.1.01 2018_10_17
+	レビューをもとに修正。
+	TableSize を TABLESIZEに変更
+	goto文を追記、EndFanctionタグを追加。
+	それに伴いfor文の中のreturn 0を消去。
+	入力時のscanfをfgetsに変更。それに伴い見直し.
+	二重for文を解消
+*/
 #include<stdio.h>
-#define TableSize 3 //おまけでテーブルの可変化を実装
+#include<stdlib.h>
+#define TABLESIZE 3 //おまけでテーブルの可変化を実装
+#define INPUTSIZE 3
+#define BUFFSIZE INPUTSIZE*(INPUTSIZE+1)+1
 
 int main() {
 	//変数宣言。tblでマップフィールドを表現
 	int i, j;
-	int tbl[TableSize][TableSize];
+	int tbl[TABLESIZE][TABLESIZE];
+	char buff[BUFFSIZE];
 
 	//データ入力受付
-	for (i = 0; i < TableSize; i++) {
-		for (j = 0; j < TableSize; j++) {
-			scanf("%d", &tbl[i][j]);
-		}
+	for (i = 0; i < TABLESIZE; i++) {
+		fgets(buff, BUFFSIZE, stdin);
+		sscanf(buff, "%d %d %d", &tbl[i][0], &tbl[i][1], &tbl[i][2]);
 	}
 
 	//一致の条件は(a(i)+b(j))-(a(i+1)+b(j))と
 	//(a(i)+b(j+1))-(a(i+1)+b(j+1))を比較すれば求められるから以下の通りになる
-	for (i = 0; i < TableSize - 1; i++) {
-		for (j = 0; j < TableSize - 1; j++) {
+	for (i = 0; i < TABLESIZE - 1; i++) {
+		for (j = 0; j < TABLESIZE - 1; j++) {
 			//条件を満たせなければ失敗とみなして終了する
 			if (tbl[i][j] - tbl[i + 1][j] != tbl[i][j + 1] - tbl[i + 1][j + 1]) {
 				printf("No\n");
-				return 0;
+				goto EndFanction;
 			}
 		}
 	}
-
 	printf("Yes\n");
+
+EndFanction:
 	return 0;
 }
 
@@ -40,18 +56,18 @@ int main() {
 //
 //int main() {
 //	int i, j, k;
-//	int a[TableSize], b[TableSize];
-//	int tbl[TableSize][TableSize];
-//	int *tbl2[TableSize];
-//	for (i = 0; i < TableSize; i++) {
+//	int a[TABLESIZE], b[TABLESIZE];
+//	int tbl[TABLESIZE][TABLESIZE];
+//	int *tbl2[TABLESIZE];
+//	for (i = 0; i < TABLESIZE; i++) {
 //			scanf("%d %d %d", &tbl[i][0],&tbl[i][1],&tbl[i][2]);
 //			tbl2[i] = tbl[i];
 //	}
 //	
 //	ZeroSerch(tbl2,a,b);
 //
-//	for (i = 0; i < TableSize; i++) {
-//		for (j = 0; j < TableSize; j++) {
+//	for (i = 0; i < TABLESIZE; i++) {
+//		for (j = 0; j < TABLESIZE; j++) {
 //			if (a[i] != 0 && b[j] != 0) {
 //				
 //			}
@@ -64,8 +80,8 @@ int main() {
 //
 //void ZeroSerch(int **tblData, int *aTbl, int *bTbl) {
 //	int i, j;
-//	for (i = 0; i < TableSize; i++) {
-//		for (j = 0; j < TableSize; j++) {
+//	for (i = 0; i < TABLESIZE; i++) {
+//		for (j = 0; j < TABLESIZE; j++) {
 //			if (tblData[i][j] == 0) {
 //				aTbl[i] = 0;
 //				bTbl[j] = 0;
--- 97991_ABC110B_StringTransformation.c
+++ 97991_ABC110B_StringTransformation.c
@@ -1,42 +1,69 @@
+/*
+	作成者:97991_片山翔太
+	作成日:2018_10_18
+*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+//環境で固定された変数を宣言
+#define IN 200000
+#define INPUTSIZE IN+2
 
 int main() {
 	//変数宣言
-	int i, j, k;
-	int flag;
-	char *Sin, *Tin, Input;
+	int i, j;
+	char Sin[INPUTSIZE], Tin[INPUTSIZE];
+	char c;
 	
 	//入力受付
-	scanf("%s", &Input);
-	int size = (sizeof(char) *Input)+1;
-	Sin = malloc(size);
-	strcpy(Sin, &Input);
+	fgets(Sin, INPUTSIZE, stdin);
+	fgets(Tin, INPUTSIZE, stdin);
+	int size = strlen(Sin);
 
-	scanf("%s", &Input);
-	Tin = malloc(size);
-	strcpy(Tin, &Input);
+	//fgetsの仕様のために改行コードを終端文字に変換
+	if (Sin[size - 1] == '\n'&&Tin[size - 1] == '\n') {
+		Sin[size - 1] = '\0';
+		Tin[size - 1] = '\0';
+	}
 
-	for(i = 0; i < size; i++) {
+	//文字列交換
+	for (i = 0; i < size; i++) {
 		if (Sin[i] != Tin[i]) {
-			for (j = i + 1, flag = 1; j < size; j++) {
-				if (Tin[i] == Tin[j] && Sin[i] != Sin[j]) {
-					printf("No\n");
-					return 0;
+			c = Sin[i];
+			for (j = 0; j < size; j++) {
+				if (Sin[j] == c || Sin[j] == Tin[i]) {
+					if (j < i) goto MyEndFlag;
+					if (Sin[j] == c) Sin[j] = Tin[i];
+					else if(Sin[j] == Tin[i]) Sin[j] = c;
+					////代入後に判定
+					//if (j <= i && Sin[j] != Tin[j])　goto MyEndFlag;
 				}
-				else if (Tin[i] == Tin[j] && Sin[i] == Sin[j]) {
-					strcpy(Tin+j, Sin+j);
-					flag++;
-				}
-			}
-			if (flag > 1) { 
-				strcpy(Tin + i, Sin + i);
-				flag = 0;
 			}
 		}
 	}
-	printf("%s", Tin);
-	printf("Yes\n");
+	//プログラムの終了
+	MyEndFlag:
+	printf(i == size ? "Yes\n":"No\n");
 	return 0;
 }
+
+//↓↓↓　没自作コード　↓↓↓
+
+//for (i = 0; i < size; i++) {
+//	if (Sin[i] != Tin[i]) {
+//		for (j = i + 1, flag = 1; j < size; j++) {
+//			if (Tin[i] == Tin[j] && Sin[i] != Sin[j]) {
+//				printf("No\n");
+//				return 0;
+//			}
+//			else if (Tin[i] == Tin[j] && Sin[i] == Sin[j]) {
+//				strcpy(Tin + j, Sin + j);
+//				flag++;
+//			}
+//		}
+//		if (flag > 1) {
+//			strcpy(Tin + i, Sin + i);
+//			flag = 0;
+//		}
+//	}
+//}
